db1354948644e841f3ef392dab0275a25a61905c
Eric D. Schabell <eric@schabell.org>
2017-06-23 16:24:29 +0200

    Addressed tech review comments for this demo.

--- init.bat
+++ init.bat
@@ -10,9 +10,9 @@ set JBOSS_HOME=%PROJECT_HOME%target\jboss-eap-7.0
 set SERVER_DIR=%JBOSS_HOME%\standalone\deployments
 set SERVER_CONF=%JBOSS_HOME%\standalone\configuration\
 set SERVER_BIN=%JBOSS_HOME%\bin
-set SRC_DIR=%PROJECT_HOME%\installs
-set SUPPORT_DIR=%PROJECT_HOME%\support
-set PRJ_DIR=%PROJECT_HOME%\projects
+set SRC_DIR=%PROJECT_HOME%installs
+set SUPPORT_DIR=%PROJECT_HOME%support
+set PRJ_DIR=%PROJECT_HOME%projects
 set BPMS=jboss-bpmsuite-6.4.0.GA-deployable-eap7.x.zip
 set EAP=jboss-eap-7.0.0-installer.jar
 set VERSION=6.4
@@ -67,14 +67,14 @@ if exist %JBOSS_HOME% (
          echo - removing existing JBoss product install...
          echo.
         
-         rmdir /s /q "%JBOSS_HOME%"
+         rmdir /s /q %JBOSS_HOME%
  )
 
 
 REM Run installers.
 echo EAP installer running now...
 echo.
-call java -jar "%SRC_DIR%/%EAP% %SUPPORT_DIR%\installation-eap" -variablefile "%SUPPORT_DIR%\installation-eap.variables"
+call java -jar %SRC_DIR%/%EAP% %SUPPORT_DIR%\installation-eap -variablefile %SUPPORT_DIR%\installation-eap.variables
 
 
 if not "%ERRORLEVEL%" == "0" (
@@ -102,18 +102,18 @@ echo.
 call %JBOSS_HOME%\bin\add-user.bat -a -r ApplicationRealm -u erics -p bpmsuite1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
 echo. 
 
-mkdir "%SERVER_BIN%\.niogit\"
-xcopy /Y /Q /S "%SUPPORT_DIR%\bpm-suite-demo-niogit\*" "%SERVER_BIN%\.niogit\"
+mkdir %SERVER_BIN%\.niogit\
+xcopy /Y /Q /S %SUPPORT_DIR%\bpm-suite-demo-niogit\* %SERVER_BIN%\.niogit\
 echo. 
 
 echo - setting up standalone.xml configuration adjustments...
 echo.
-xcopy /Y /Q "%SUPPORT_DIR%\standalone.xml" "%SERVER_CONF%"
+xcopy /Y /Q %SUPPORT_DIR%\standalone.xml %SERVER_CONF%
 echo.
 
 echo - setup email task notification users...
 echo.
-xcopy /Y /Q "%SUPPORT_DIR%\userinfo.properties" "%SERVER_DIR%\business-central.war\WEB-INF\classes\"
+xcopy /Y /Q %SUPPORT_DIR%\userinfo.properties %SERVER_DIR%\business-central.war\WEB-INF\classes\
 
 echo.
 echo ========================================================================
